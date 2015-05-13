e0faa2b3a01e77f757a5a80a0b0c5ffdf1203865
pjaey <shahp617@gmail.com>
2015-05-13 05:56:30 -0400

    waiting for 1s before the links are grabbed from the webpage manifest.json->content_scripts->run_at=document_idle doesn't let the webpage load completely this change will let the webpage have an extra second after window.onload() is fired

--- content.js
+++ content.js
@@ -1,47 +1,52 @@
-/* Get all links */
-var hrefs = [], all_links = document.links;
-for (var i = 0; i < all_links.length; i++) {
-    hrefs.push(all_links[i].href);
-}
-/* extract pattern from the links */
-var pattern = ('^https{0,1}://stackoverflow\.com/questions/([0-9]*?)/'),
-    patterns = [],
-    found = false;
-for (var i = 0; i < hrefs.length; i++) {
-    var match = hrefs[i].match(pattern);
-    if (match !== null && match.length >= 2) {
-        patterns.push(match[1]);
-    }
-}
-/* retain only unique entries */
-var seen = {},
-    unique_patterns = patterns.filter(function (item) {
-        return seen.hasOwnProperty(item) ? false : (seen[item] = true);
-    });
+ /* let the page load completely */
+setTimeout(execute, 1000);
 
-/* make an api call */
-if (unique_patterns.length >= 1) {
-    var query_param, chunk = 100;
-    for (var i = 0, j = unique_patterns.length; i < j; i += chunk) {
-        query_param = unique_patterns.slice(i, i + chunk).join(';');
-        jQuery.getJSON('https://api.stackexchange.com/2.2/questions/' + 
-            query_param +
-            '?order=desc&sort=votes&site=stackoverflow',
-            function (json) {
-                if (json.hasOwnProperty('items')) {
-                    after_api_call(json['items']);
-                } else {
-                    after_api_call([]);
+function execute() {
+    /* Get all links */
+    var hrefs = [], all_links = document.links;
+    for (var i = 0; i < all_links.length; i++) {
+        hrefs.push(all_links[i].href);
+    }
+    /* extract pattern from the links */
+    var pattern = ('^https{0,1}://stackoverflow\.com/questions/([0-9]*?)/'),
+        patterns = [],
+        found = false;
+    for (var i = 0; i < hrefs.length; i++) {
+        var match = hrefs[i].match(pattern);
+        if (match !== null && match.length >= 2) {
+            patterns.push(match[1]);
+        }
+    }
+    /* retain only unique entries */
+    var seen = {},
+        unique_patterns = patterns.filter(function (item) {
+            return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+        });
+
+    /* make an api call */
+    if (unique_patterns.length >= 1) {
+        var query_param, chunk = 100;
+        for (var i = 0, j = unique_patterns.length; i < j; i += chunk) {
+            query_param = unique_patterns.slice(i, i + chunk).join(';');
+            jQuery.getJSON('https://api.stackexchange.com/2.2/questions/' + 
+                query_param +
+                '?order=desc&sort=votes&site=stackoverflow',
+                function (json) {
+                    if (json.hasOwnProperty('items')) {
+                        process_results(json['items']);
+                    } else {
+                        process_results([]);
+                    }
                 }
-            }
-        );
-    }  
-} else {
-    after_api_call([]);
+            );
+        }  
+    } else {
+        process_results([]);
+    }
 }
 
 /* talk with background and popup */
-function after_api_call(json_list) {
+function process_results(json_list) {
     /* render number of links on the batch/icon */
     chrome.runtime.sendMessage({        
         from: 'content',       
@@ -54,4 +59,4 @@ function after_api_call(json_list) {
             response(json_list);
         }
     });
-}  
+} 
